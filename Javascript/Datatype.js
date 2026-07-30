a2be1fe59da4b5d142d779d6ab4058f8683011c2
MonikaR1990 <monicaselas2010@gmail.com>
2026-07-30 15:05:06 +0530

    July 30

---
+++ Javascript/Datatype.js
@@ -0,0 +1,57 @@
+//string
+let name = "Bala"
+let data = " "
+let nums = "123456"
+let val = "####%%%%"
+
+//number
+let age = 12
+let price = 12.45
+
+//boolean
+
+let isActive = true 
+
+
+//camel Case (varibles, methods or functions, object)
+let empName
+
+//Pascal Case
+//class EmployeeDetails (class. interface)
+
+
+//types of datatypes
+//string
+//number
+//boolean
+//undefined
+//null
+//BigInt
+//Symbol
+
+
+let x
+console.log(x)
+
+let y = null
+console.log(y)
+
+let population = 255454435345376776n
+console.log(population)
+
+let id = Symbol("Emp Id"); //Description for a variable
+
+console.log(id)
+
+let id1 = Symbol("Voter ID")
+let id2 = Symbol("Voter ID")
+
+console.log(id1===id2)
+
+
+
+
+
+
+
+
---
+++ Javascript/Variables.js
@@ -0,0 +1,62 @@
+//Variables
+
+let name = "Bala"
+let age = 12
+let isActive = true
+
+//var
+//let
+//const
+
+//var ename; //Declaration
+
+var ename = "Meena" //Initialization
+
+ename = "Mathi" //Re-assign
+
+var ename = "Muthu" //Re-Declaration
+
+console.log(ename)
+
+//let
+
+let sname = "Beena"
+
+sname = "Latha" //Re-assign
+
+//let sname = "Banu" //Re-Declaration
+
+console.log(sname)
+
+//const
+
+const cname = "Jothi" //Can't Re-assign
+
+//cname = "Mathi"  //Can't Re-Declare
+
+
+console.log(cname)
+
+{
+    const price = 10
+
+    console.log(price)
+
+}
+//console.log(price)  
+
+
+console.log(x) //hoisting
+
+const x = 10
+
+var a;
+let b;
+
+a = 10
+
+b = 20
+
+const c = 30
+
+
